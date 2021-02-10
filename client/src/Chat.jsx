78acc57b01b5206e10acb74ce089554705b3f973
Amman Abua <ammanabua@gmail.com>
2021-02-10 12:08:15 +0100

    add homepage app to use module federation

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, useSubscription, gql, useMutation } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+
 import { Container, Row, Col, FormInput, Button } from "shards-react";
 
+const link = new WebSocketLink({
+    uri: `ws://localhost:4000/`,
+    options: {
+      reconnect: true
+    }
+  });
+
 const client = new ApolloClient({
+    link,
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
 });
 
 const GET_MESSAGES = gql`
-    query {
+    subscription {
         messages {
             id
             content
@@ -23,9 +33,7 @@ const POST_MESSAGE = gql`
     }`;
 
 const Messages = ({ user }) => {
-    const { data } = useQuery(GET_MESSAGES, {
-        pollInterval: 500,
-    });
+    const { data } = useSubscription(GET_MESSAGES);
     if (!data) {
         return null;
     }
